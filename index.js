a899ec33f6d9db60b2156b56f2b8ebe9542e0f14
nepalrohan <rohannepal098@gmail.com>
2025-03-15 17:13:02 +0545

    explore emit on methods and create a simple chat app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import express from "express";
 import http from 'http'
-import socketIo from "socket.io";
+import {Server} from "socket.io";
 
 const app=express();
 
@@ -10,6 +10,52 @@ const server = http.createServer(app);
 
 //initialize socket io
 
-const io = socketIo(server);
+const io =new  Server(server);
 
-app.use(express.static(' public'));
+app.use(express.static('public'));
+
+
+const users = new Set();
+
+io.on('connection', (socket)=>{
+    console.log(' user connected');
+//handle user when they join the chat
+    socket.on('join', (userName)=>{
+        users.add(userName);
+
+        socket.userName=userName;
+
+        //broadcast to all user that a new user join
+io.emit('userJoined', userName);
+
+//send updated userlist to clients
+io.emit('userList', Array.from(users))
+    })
+
+socket.on('chatMessage', (message)=>{
+    //broadcast the received message to all connected clients
+
+    io.emit('chatMessage', message)
+})
+
+
+socket.on('disconnect', ()=>{
+    console.log('A user has disconnected');
+    users.forEach(user=>{
+        if(user === socket.userName){
+            users.delete(user);
+            io.emit('userleft', user);
+            io.emit('userList', Array.from(users));
+        }
+    })
+})
+
+})
+
+
+
+
+const port = 3000;
+server.listen(port, ()=>{
+    console.log('server is running')
+})
